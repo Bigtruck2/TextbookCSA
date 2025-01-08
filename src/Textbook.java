8608eacb6e94a5c3c63c07c45ac8ed1eeaee8acd
Bigtruck2 <bigburrito363@gmail.com>
2025-01-08 11:34:51 -0500

    second

--- src/Textbook.java
+++ src/Textbook.java
@@ -1,2 +1,18 @@
-public class Textbook {
+public class Textbook extends Book{
+    private int edition;
+    public Textbook(String bookTitle, double bookPrice, int edition) {
+        super(bookTitle, bookPrice);
+        this.edition = edition;
+    }
+
+    @Override
+    public String getBookInfo() {
+        return super.getBookInfo()+"-"+edition;
+    }
+    public boolean canSubstituteFor(Textbook book){
+        return book.getEdition()<edition &&getTitle().equals(book.getTitle());
+    }
+    public int getEdition() {
+        return edition;
+    }
 }
